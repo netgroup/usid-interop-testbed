71ec2776c3e3ee8e8c70fe09f8aa44cd9d948ab9
The ROSE project <rose_project@rose_project.org>
2020-06-21 10:44:13 +0200

    modified p4 startup scripts

--- nets/conext-usid/nodeconf/p4_1/start.sh
+++ nets/conext-usid/nodeconf/p4_1/start.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
 BASE_DIR=nodeconf
-NODE_NAME=r6
-FRR_PATH=/usr/lib/frr
+NODE_NAME=p4_1
+THRIFT_PORT=4001
 
-#enable IPv4 forwarding
-#sysctl -w net.ipv4.ip_forward=1
-sysctl -w net.ipv6.conf.all.forwarding=1
-#disable reverse path filtering (needed for dynamic routing)
-#sysctl -w net.ipv4.conf.all.rp_filter=0
-#sysctl -w net.ipv4.conf.default.rp_filter=0
-#the following for loop also disables all and default
-#for i in /proc/sys/net/ipv4/conf/*/rp_filter ; do
-#  echo 0 > $i 
-#done
+simple_switch --thrift-port $THRIFT_PORT -i 0@$NODE_NAME-r2 -i 1@$NODE_NAME-p4_2 -i 2@$NODE_NAME-vpp_3 -i 3@$NODE_NAME-sw $PWD/$BASE_DIR/$NODE_NAME/bmv2.json &
 
-
-echo "no service integrated-vtysh-config" >> /etc/frr/vtysh.conf
-chown frr:frrvty $BASE_DIR/$NODE_NAME
-#chown quagga:quagga $BASE_DIR/$NODE_NAME
-
-#ip address restore < $BASE_DIR/$NODE_NAME/$NODE_NAME.address
-$FRR_PATH/zebra -f $PWD/$BASE_DIR/$NODE_NAME/zebra.conf -d -z $PWD/$BASE_DIR/$NODE_NAME/zebra.sock -i $PWD/$BASE_DIR/$NODE_NAME/zebra.pid
-
-sleep 1
-
-#ip -6 route restore < $BASE_DIR/$NODE_NAME/$NODE_NAME.route6
-$FRR_PATH/isisd -f $PWD/$BASE_DIR/$NODE_NAME/isisd.conf -d -z $PWD/$BASE_DIR/$NODE_NAME/zebra.sock -i $PWD/$BASE_DIR/$NODE_NAME/isisd.pid
-
-# enable Segment Routing for IPv6
-sysctl -w net.ipv6.conf.all.seg6_enabled=1
-for dev in $(ip -o -6 a | awk '{ print $2 }' | grep -v "lo")
-do
-   sysctl -w net.ipv6.conf.$dev.seg6_enabled=1
-done
+# simple_switch_CLI --thrift-port $THRIFT_PORT < $PWD/$BASE_DIR/$NODE_NAME/commands.txt
